93678cb59b8a4dce66c36d97debd56bbc4cd1004
luccas <luccasfigueiragc@gmail.com>
2025-05-17 19:45:32 -0300

    atualizando com base figma

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,15 @@
 const imagens = [
-  "src/assets/imgs/carrogif.gif",
+  "src/assets/imgs/carroe3.webp",
   "src/assets/imgs/carroe4.jpg",
   "src/assets/imgs/carroeletrico.jpeg"
 ];
 
 let indice = 0;
 
-// Criar elemento <img> dentro da hero-bg
-const imgElement = document.createElement("img");
-imgElement.src = imagens[indice];
-document.querySelector(".hero-bg").appendChild(imgElement);
-
 function trocarFundo() {
-  imgElement.src = imagens[indice];
+  document.querySelector(".hero-bg").style.backgroundImage = `url('${imagens[indice]}')`;
   indice = (indice + 1) % imagens.length;
 }
 
-setInterval(trocarFundo, 3000);
+trocarFundo();
+setInterval(trocarFundo, 5000);
